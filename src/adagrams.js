155ef5fcb25d59fa9d14a7ad455381da0491e1ca
Sarah Koch <sarahkoch810@gmail.com>
2024-11-22 13:27:05 -0800

    Ported Adagrams code from Python to JS. Passes all Wave 3 tests.

--- src/adagrams.js
+++ src/adagrams.js
@@ -29,7 +29,26 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const POINT_VALUES  = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 
+    'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 
+    'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 
+    'Y': 4, 'Z': 10
+  }
+  const bonusLength = 7;
+  const bonusPoints = 8;
+  let totalScore = 0;
+
+  word = word.toUpperCase();
+  for (const letter of word) {
+    totalScore += POINT_VALUES[letter]
+  }
+  
+  if (word.length >= bonusLength) {
+    totalScore += bonusPoints;
+  }
+
+  return totalScore;
 };
 
 export const highestScoreFrom = (words) => {
