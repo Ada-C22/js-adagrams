b3f94222d539f2642ac963b85cff2fcf44504c5a
Tami <tamigaertner@outlook.com>
2024-11-22 11:53:01 -0500

    work wave2 and fixed bugs

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,17 +1,15 @@
 import letterPool  from './letterPool.js';
 import scoreChart from './scoreChart.js';
 
-console.log(scoreChart)
-console.log(letterPool)
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   let hand = []
   let letterCounts = { ...letterPool};
-  let availabeLetters = [];
-  while (hand.length <10 ){
-    
 
-   
+  while (hand.length <10 ){
+    let availabeLetters = [];
+
     for (let letter in letterCounts){
       if (letterCounts[letter]> 0) {
         availabeLetters.push(letter);
@@ -30,10 +28,20 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  let word = input.toUpperCase()
+  let handCopy = [...lettersInHand];
 
-
-
+  for (let letter of word){
+    let index = handCopy.indexOf(letter);
+    if (index === -1){
+      return false;
+    }
+    handCopy.splice(index, 1);
+  }
+  return true;
 };
+       
+   
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
