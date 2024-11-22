cef9c22ce76a23e97c7d8d3e7f58c7a1d1bae064
Priyanka <pri.ppk@gmail.com>
2024-11-22 14:27:56 -0800

    Add wave 3 implementation. all tests for wave 3 and above pass.

--- src/adagrams.js
+++ src/adagrams.js
@@ -68,7 +68,34 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreDict = {
+    A: 1, E: 1, I: 1, O: 1, U: 1,
+    L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+  };
+
+  if (word === '') {
+    return 0;
+  }
+
+  const upperWord = word.toUpperCase();
+  let score = 0;
+
+  for (let i = 0; i < upperWord.length; i++) {
+    const char = upperWord[i];
+    if (char in scoreDict) {
+      score += scoreDict[char];
+    }
+  }
+  if ([7,8,9,10].includes(upperWord.length)) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
