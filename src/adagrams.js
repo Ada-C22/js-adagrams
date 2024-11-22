dda1517f5b23176f7d551c4b90639d0df75605a7
Beenish <beenishali693@gmail.com>
2024-11-22 14:37:08 -0800

    passing wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,4 +1,4 @@
-const letterPool = {
+const LETTER_POOL = {
   'A': 9, 
   'B': 2, 
   'C': 2, 
@@ -26,7 +26,34 @@ const letterPool = {
   'Y': 2, 
   'Z': 1
 };
-
+const SCORE_CHART = {  
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 3, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10            
+}
 
 
 const letters = [
@@ -76,7 +103,7 @@ export const drawLetters = () => {
       } else {
         letterFreq[randomLetter] = 1;
       }
-    if (letterFreq[randomLetter] <= letterPool[randomLetter]){
+    if (letterFreq[randomLetter] <= LETTER_POOL[randomLetter]){
       drawnLetters.push(randomLetter);
       i++
     }
@@ -99,10 +126,22 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0;
+
+  for (let letter of word){
+    letter = letter.toUpperCase()
+    score += SCORE_CHART[letter];
+  }
+
+  if (word.length >= 7){
+    score += 8;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
 
+console.log(scoreWord(''))
