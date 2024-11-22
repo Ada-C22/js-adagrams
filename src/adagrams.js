0f0c27fbab1183bc860f8db94d8217624bc4e9d6
Tami <tamigaertner@outlook.com>
2024-11-21 19:41:40 -0500

    work wave 1, added lettPool and scoreCard folder and set up folder

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,38 @@
+import letterPool  from './letterPool.js';
+import scoreChart from './scoreChart.js';
+
+console.log(scoreChart)
+console.log(letterPool)
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let hand = []
+  let letterCounts = { ...letterPool};
+  let availabeLetters = [];
+  while (hand.length <10 ){
+    
+
+   
+    for (let letter in letterCounts){
+      if (letterCounts[letter]> 0) {
+        availabeLetters.push(letter);
+      }
+    }
+    if (availabeLetters.length === 0){
+      break
+    }
+    let index = Math.floor(Math.random() * availabeLetters.length);
+    let chooseLetter = availabeLetters[index]
+    letterCounts[chooseLetter] -=1;
+    hand.push(chooseLetter)
+  }
+  return hand; 
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+
+
+
 };
 
 export const scoreWord = (word) => {
