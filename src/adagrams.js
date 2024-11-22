44440f99a3da8b4ec9235c459f7390ad24f81e10
Weixi He <heweixiguge@gmai.com>
2024-11-22 16:57:40 -0500

    finish wave 3 and complete miss test

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,6 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  // create an object to store letter
   const letterDist = {
     A: 9,
     B: 2,
@@ -28,17 +29,18 @@ export const drawLetters = () => {
     Y: 2,
     Z: 1
   };
-
+  // change object to array
   let letterList = []
   for (let [letter, count] of Object.entries(letterDist)) {
   letterList.push(...Array(count).fill(letter));
   }
-
+  // defind result and copy a temporary array
   let result = []
   const tempList = [...letterList];
-
+  // use loop to Random pick 10 letter 
   for (let i = 0; i < 10; i++) {
     let randomIndex = Math.floor(Math.random() * tempList.length);
+    // append letter in result and drop uesd one
     result.push(tempList[randomIndex]);
     tempList.splice(randomIndex, 1);
   }
@@ -52,7 +54,7 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   const inHandUpper = lettersInHand.map(letter => letter.toUpperCase());
   const inputUpper = input.toUpperCase();
 
-  // create Object for lettersInHand  
+  // create new Object for lettersInHand  
   let letterCount = {};
   for (let letter of inHandUpper) {
     if (letter in letterCount) {
@@ -61,9 +63,9 @@ export const usesAvailableLetters = (input, lettersInHand) => {
     letterCount[letter] = 1;
   }  
 }
-  // create Object for input
+  // create new Object for input
   let newInput = {}
-  for (const letter of inputUpper) {
+  for (let letter of inputUpper) {
     if (letter in newInput) {
       newInput[letter] += 1;
     } else {
@@ -85,6 +87,27 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  // create an object to store letter and values 
+  const letterValues = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+  };
+  let score = 0;
+
+  if (!word || word.length === 0) {
+    return score;
+  }
+  for (let letter of word) {
+    score += letterValues[letter.toUpperCase()];
+    }
+    if (word.length >= 7) {
+      score += 8;}
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
