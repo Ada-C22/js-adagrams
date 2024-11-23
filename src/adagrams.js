7495d7dced3ae73127b9b1ef66992da2a5f47e08
Aksana <adziamidava@gmail.com>
2024-11-22 18:44:30 -0800

    created letterPool object, implemented function to build hand with 10 letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,109 @@
+const letterPool = {
+  A: 9, 
+  B: 2, 
+  C: 2, 
+  D: 4, 
+  E: 12, 
+  F: 2, 
+  G: 3, 
+  H: 2, 
+  I: 9, 
+  J: 1, 
+  K: 1, 
+  L: 4, 
+  M: 2, 
+  N: 6, 
+  O: 8, 
+  P: 2, 
+  Q: 1, 
+  R: 6, 
+  S: 4, 
+  T: 6, 
+  U: 4, 
+  V: 2, 
+  W: 2, 
+  X: 1, 
+  Y: 2, 
+  Z: 1
+}
+
+const scoreObj = {
+  A: 1, 
+  B: 3, 
+  C: 3, 
+  D: 2, 
+  E: 1, 
+  F: 4, 
+  G: 2, 
+  H: 4, 
+  I: 1, 
+  J: 8, 
+  K: 5, 
+  L: 1, 
+  M: 3, 
+  N: 1, 
+  O: 1, 
+  P: 3, 
+  Q: 10, 
+  R: 1, 
+  S: 1, 
+  T: 1, 
+  U: 1, 
+  V: 4, 
+  W: 4, 
+  X: 8, 
+  Y: 4, 
+  Z: 10
+}
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+const letterArray = [];
+for (const [letter, freq] of Object.entries(letterPool)) {
+  for (let num = 0; num < freq; num++) {
+    letterArray.push(letter);
+  };
+}
+// console.log(letterArray)
+
+const hand = [];
+const letterArrayLen = letterArray.length;
+const letterCount  = {};
+
+while (hand.length < 10 ){
+  let index = Math.floor(Math.random()*letterArrayLen);
+  let newLetter = letterArray[index];
+  if (!letterCount[newLetter]) {
+    letterCount[newLetter] = 0;
+  }
+  
+  if (letterCount[newLetter] < letterPool[newLetter]){
+    hand.push(newLetter);
+    letterCount[newLetter] +=1;
+  }
+}
+console.log(hand)
+return hand;
 };
+// Implement this method for wave 1
 
 export const usesAvailableLetters = (input, lettersInHand) => {
+  let copyLettersInHand = lettersInHand;
+  let inputUp = input.toUpperCase();
+
+  for (const letter of inputUp) {
+    if (!copyLettersInHand.includes(letter)){
+      return false;
+    }
+    //  else if copyLettersInHand.icludes(letter) 
+
+
+  }
+  
+  
+
+
+
+
   // Implement this method for wave 2
 };
 
