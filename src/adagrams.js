989c09be81bef9a814a59130257d184ddddd317a
Leidy <leidymarbu@gmail.com>
2024-11-24 17:24:51 -0500

    Finished wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -67,3 +67,30 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
   return true;
 };
+
+export const scoreWord = (word) => {
+
+  const SCORE_DICT = {  'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 
+    's': 1, 't': 1, 'd': 2, 'g': 2, 'b': 3, 'c': 3, 'm': 3, 'p': 3, 
+    'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8, 
+    'q': 10, 'z': 10 }
+  
+  let score = 0;
+  
+  if (!word) { 
+    return score;
+  }
+  
+  const lowerCaseWord = word.toLowerCase();
+  for (let letter of lowerCaseWord) {
+      if(SCORE_DICT[letter]){ // Check if the letter exists in SCORE_DICT
+      score += SCORE_DICT[letter];
+    }
+  }
+  
+  if (lowerCaseWord.length > 6){
+    score += 8;
+    }
+    return score;
+  };
+  
