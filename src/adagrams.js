31bd4751ebb72948de00ee7c904b379f64a0db5d
Corntto9 <>
2024-11-26 05:36:12 +0800

    wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,9 +26,10 @@ const letterPool = {
   'Y': 2, 
   'Z': 1
 }
-console.log(letterPool)
+
 export const drawLetters = () => {
   const allLetters = [];
+  
   // pushed all letters into on arr. 
   for (const [letter, count] of Object.entries(letterPool)) {
     for (let i = 0; i < count; i++) {
@@ -45,14 +46,79 @@ export const drawLetters = () => {
     allLetters.splice(randomIndex,1); // remove used letter.
   }
   return hand;
-};
+}
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+  // count the letter freq in letters in hand
+  const count = {};
+  for (const letter of lettersInHand) {
+    if (count[letter]) {
+      count[letter]++;
+    } else {
+      count[letter] = 1;
+    }
+  } 
+  
+  for (const letter of input) {
+    if (letter in count) {
+      if (count[letter] > 0) {
+          count[letter]--;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+  return true;
+}
+
+
+const scoreChart = {
+  'A': 1,
+  'E': 1,
+  'I': 1,
+  'O': 1,
+  'U': 1,
+  'L': 1,
+  'N': 1,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'D': 2,
+  'G': 2,
+  'B': 3,
+  'C': 3,
+  'M': 3,
+  'P': 3,
+  'F': 4,
+  'H': 4,
+  'V': 4,
+  'W': 4,
+  'Y': 4,
+  'K': 5,
+  'J': 8,
+  'X': 8,
+  'Q': 10,
+  'Z': 10,
+}
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0;
+  
+  if (word.length >= 7) {
+    score += 8;
+  }
+
+  for (let letter of word.toUpperCase()) {
+    
+    if (letter in scoreChart) {
+      score += scoreChart[letter];
+    }
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
