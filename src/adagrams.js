592dad5ae7bb9d744be196f4ce0e19ea7afafdb6
Tatiana Snook <tatsnook@gmail.com>
2024-11-24 19:58:35 -0600

    wave3 completed scoreWord function and its tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -61,13 +61,52 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+const scoreChart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
+
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let points = 0;
+  word = word.toUpperCase();
+
+  for (let letter of word) {
+    points += scoreChart[letter];
+  }
+
+  if (word.length >= 7) {
+    points += 8
+  }
+  return points;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
 
-const lettersInHand = drawLetters();
-usesAvailableLetters('apple', lettersInHand);
+
+console.log(scoreWord('dog'));
