d9453b8d3380867c387ed04cd912f88d2e23ddd4
Elzara Turakulova <endjik@gmail.com>
2024-11-24 22:38:04 -0800

    wave 3: completed scoreWord function, refactored drawLetters function, completed a test

--- src/adagrams.js
+++ src/adagrams.js
@@ -30,14 +30,13 @@ export const drawLetters = () => {
   };
   const letterBank = [];
   const workingPool = { ...letterPool };
-  for (let i = 0; i < 10; i++) {
+  let i = 0
+  while (i < 10) {
     const letter = getWeightedRandom(workingPool);  
     if (workingPool[letter] > 0) {
       letterBank.push(letter);
       workingPool[letter]--;
-    } else {
-      i--;
-      continue;
+      i++;
     }
   }
   return letterBank;
@@ -78,8 +77,47 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
   return true;
 };
+
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const lettersValue = {
+   'A': 1,
+   'B': 3,
+   'C': 3,
+   'D': 2,
+   'E': 1,
+   'F': 4, 
+   'G': 2,
+   'H': 4,
+   'I': 1,
+   'J': 8,
+   'K': 5,
+   'L': 1,
+   'M': 3,
+   'N': 1,
+   'O': 1,
+   'P': 3,
+   'Q': 10,
+   'R': 1,
+   'S': 1,
+   'T': 1,
+   'U': 1,
+   'V': 4,
+   'W': 4,
+   'X': 8,
+   'Y': 4,
+   'Z': 10
+};
+  let score = 0;
+  for (const letter of word.toUpperCase()) {
+    score += lettersValue[letter];
+  }
+
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
