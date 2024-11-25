3a9367da9ae469abd607fcee190e5456c6595c48
Elzara Turakulova <endjik@gmail.com>
2024-11-24 20:41:25 -0800

    wave 1: added getWeightedRandom helper function and letter pool

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,50 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+
+
 };
+
+const getWeightedRandom = (object) => {
+  const total = Object.values(object).reduce((sum, weight) => sum + weight);
+  const random = Math.random() * total;
+  
+  let steppingSum = 0;
+
+  for (const [letter, weight] of Object.entries(object)) {
+    steppingSum += weight;
+    if (steppingSum > random) {
+      return letter;
+    }
+  }
+}
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
