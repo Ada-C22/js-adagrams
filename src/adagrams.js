51f645b58d4029edacda4df83e8627fe8e83b69a
marjanak <marjana1816@gmail.com>
2024-11-24 20:51:32 -0800

    Dove with wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,9 +27,35 @@ const letterPool =
         'Z':1
     }
 
-
-
-
+	const score = {
+		'A' : 1,
+		'E' : 1,
+		'I' : 1,
+		'O' : 1,
+		'U' : 1,
+		'L' : 1,
+		'N' : 1,
+		'R' : 1,
+		'S' : 1,
+		'T' : 1,
+		'D' : 2,
+		'G' : 2,
+		'B' : 3,
+		'C' : 3,
+		'M' : 3,
+		'P' : 3,
+		'F' : 4,
+		'H' : 4,
+		'V' : 4,
+		'W' : 4,
+		'Y' : 4,
+		'K' : 5,
+		'J' : 8,
+		'X' : 8,
+		'Q' :10,
+		'Z' :10
+	}
+	
 export const drawLetters = () => {
   // Implement this method for wave 1
     const listLetters = [];
@@ -64,6 +90,17 @@ return true;
 }
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+	let totalScore = 0;
+	word = word.toUpperCase();
+	for (let i = 0; i <word.length; i ++){
+		let letter = word [i];
+		let value = score[letter] || 0;
+		totalScore += value ;
+	}
+	if (word.length >= 7 && word.length <=10){
+		totalScore += 8;
+	}
+	return totalScore;
 };
 
 export const highestScoreFrom = (words) => {
