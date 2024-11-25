b59e42d9d00c05adcf5c8ba17a7985aa86059a13
Brianna Root <brianna.k.root@gmail.com>
2024-11-25 16:42:43 +1300

    Created functions for use available letters and score words.

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,17 @@ const letterPool = {
   'Z': 1
 };
 
+const scoreChart = {
+  'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+  'D': 2, 'G': 2,
+  'B': 3, 'C': 3, 'M': 3, 'P': 3,
+  'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+  'K': 5,
+  'J': 8, 'X': 8,
+  'Q': 10, 'Z': 10
+};
+
+
 export const drawLetters = () => {
   // Create a frequency pool 
   const letterFrequency = [];
@@ -51,16 +62,33 @@ export const drawLetters = () => {
   
 };
 
+export const usesAvailableLetters = (input, lettersInHand) => {
+  const word = input.toUpperCase();
+  const letterList = []
+  const letterBankCopy = lettersInHand.slice()
 
-console.log(drawLetters());
+    for (let letter of word) {
+      if (letter in letterBankCopy) {
+        letterList.push(letter);
+        letterBankCopy.splice(letter, 1)
+      } else return false;
+    }; 
+    return true;
+};
 
-// export const usesAvailableLetters = (input, lettersInHand) => {
-//   // Implement this method for wave 2
-// };
+export const scoreWord = (word) => {
+  const upperCaseWord = word.toUpperCase();
+  let score = 0;
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+  for (let letter of upperCaseWord) {
+    score = score + scoreChart[letter];
+  }; 
+
+  if (word.length > 6) {
+    score = score + 8;
+  }; 
+  return score;
+};
 
 // export const highestScoreFrom = (words) => {
 //   // Implement this method for wave 4
