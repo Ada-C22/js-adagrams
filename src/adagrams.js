f1faea21ea227c3d1cd6ce8f35937bb7e53b7cce
Tatiana Snook <tatsnook@gmail.com>
2024-11-24 19:25:35 -0600

    wave 1 and 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,64 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const lettersList = [];
+  const randomTenLetters = [];
+
+  for(let letter in LETTER_POOL) {
+    for(let i = 0; i < LETTER_POOL[letter]; i++) {
+      lettersList.push(letter);
+    };
+  };
+
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * lettersList.length);
+    randomTenLetters.push(lettersList[randomIndex]);
+    lettersList.splice(randomIndex, 1);
+  };
+
+  return randomTenLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const lettersInHandCopy = [... lettersInHand];
+  input = input.toUpperCase();
+
+  for (const letter of input) {
+    const index = lettersInHandCopy.indexOf(letter);
+    if (index != -1) {
+      lettersInHandCopy.splice(index, 1)
+    } else {
+      return false;
+    }
+  };
+  return true;
 };
 
 export const scoreWord = (word) => {
@@ -13,3 +68,6 @@ export const scoreWord = (word) => {
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
+
+const lettersInHand = drawLetters();
+usesAvailableLetters('apple', lettersInHand);
