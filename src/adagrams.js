520e102dc531739891a74cc071931ec808fac8fb
somy <smycho28@gmail.com>
2024-11-26 21:12:43 -0800

    wave 3 and 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,23 @@
+const letterPool = {
+  'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3,
+  'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6,
+  'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
+  'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+};  
+  
+  //Map of points 
+  const letterValue = new Map([
+    [['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 1],
+    [['D', 'G'], 2],
+    [['B', 'C', 'M', 'P'], 3],
+    [['F', 'H', 'V', 'W', 'Y'], 4],
+    [['K'], 5],
+    [['J', 'X'], 8],
+    [['Q', 'Z'], 10]
+  ]);
+
+
 export const drawLetters = () => {
-  const letterPool = {
-    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3,
-    'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6,
-    'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
-    'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
-};
 
 // Create a list of letters based on the pool
 let lettersList = [];
@@ -25,42 +38,77 @@ for (let i = 0; i < 10; i++) {
     const randomLetter = temporaryLettersList.splice(randomIndex, 1)[0];
     hand.push(randomLetter);
 }
-
 return hand;
-  // Implement this method for wave 1
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-  // Loop through each letter in the input string
   for (let i = 0; i < input.length; i++) {
     const letter = input[i];
     
-    // Check if the letter is in lettersInHand
     if (!lettersInHand.includes(letter)) {
-      return false; // Return false if any letter is not in the hand
+      return false;
     }
     
-    // If the letter is in the hand, remove it from the hand (so it can't be reused)
-    // This ensures that a letter can only be used as many times as it appears in the hand
     const index = lettersInHand.indexOf(letter);
-    lettersInHand.splice(index, 1); // Remove the letter from the hand
+    lettersInHand.splice(index, 1); 
   }
 
-  // If we made it here, it means all letters in input are available in the hand
   return true;
 };
 
 
 
-
-
-
-
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
 
+
+  if (word.length === 0){
+    return 0;
+  }
+
+  let totalScore = 0
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i].toUpperCase();
+
+    for (let [letters, score] of letterValue) {
+      if (letters.includes(letter)) {
+        totalScore += score;
+        break; 
+      }
+    }
+  }
+  if (word.length > 6){
+    totalScore += 8
+  }
+  return totalScore 
+};
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+
+  if (words.length === 0) {
+    return 0;
+  }
+
+  let winner = { word: '', score: 0 };
+
+  for (let word of words) {
+    let currentScore = scoreWord(word);
+
+    if (currentScore > winner.score) {
+      winner = { word, score: currentScore };
+    } 
+
+    else if (currentScore === winner.score) {
+      if (word.length === 10 && winner.word.length !== 10) {
+        winner = { word, score: currentScore };
+      } 
+
+      else if (
+        word.length !== 10 && 
+        winner.word.length !== 10 && 
+        word.length < winner.word.length
+      ) {
+        winner = { word, score: currentScore };
+      }
+    }
+  }
+  return winner;
 };
