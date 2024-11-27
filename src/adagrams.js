566147cd0c967d0bd5e374f303e47f8a54d4fba0
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-27 04:53:48 -0500

    refactored, updated letterPool and scoreChart to global scope

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,32 +1,62 @@
+const letterPool = {
+  'A': 9,  
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
+};
+
+const scoreChart = {
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 3, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10
+};
+
 export const drawLetters = () => {
-  const letterPool = {
-    'A': 9,  
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-  };
   const lettersAvailable = [];
   for (const [letter, quantity] of Object.entries(letterPool)) {
     for (let i = 0; i < quantity; i++) {
@@ -44,9 +74,7 @@ export const drawLetters = () => {
   return lettersDrawn;
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  input = input.toUpperCase();
-  
+export const usesAvailableLetters = (input, lettersInHand) => {  
   const letterBank = {};
   for (const letter of lettersInHand) {
     if (letterBank[letter] === undefined) {
@@ -56,7 +84,7 @@ export const usesAvailableLetters = (input, lettersInHand) => {
     }
   };
 
-  for (const letter of input) {
+  for (const letter of input.toUpperCase()) {
     if (letterBank[letter] === undefined || letterBank[letter] === 0) {
       return false;
     } else {
@@ -67,36 +95,8 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  const scoreChart = {
-    'A': 1, 
-    'B': 3, 
-    'C': 3, 
-    'D': 2, 
-    'E': 1, 
-    'F': 4, 
-    'G': 2, 
-    'H': 4, 
-    'I': 1, 
-    'J': 8, 
-    'K': 5, 
-    'L': 1, 
-    'M': 3, 
-    'N': 1, 
-    'O': 1, 
-    'P': 3, 
-    'Q': 10, 
-    'R': 1, 
-    'S': 1, 
-    'T': 1, 
-    'U': 1, 
-    'V': 4, 
-    'W': 4, 
-    'X': 8, 
-    'Y': 4, 
-    'Z': 10
-  };
   let score = 0;
-  for (const letter of word) {
+  for (const letter of word.toUpperCase()) {
     score += scoreChart[letter];
   };
 
