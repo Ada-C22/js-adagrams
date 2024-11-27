082b1c9ca8b476927a66139f06595d630a5de054
Lula <mlusan95@gmail.com>
2024-11-26 20:41:36 -0500

    wave 1 passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,57 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const hand = [];
+  const copyPool = {};
+
+  let idx = 0;
+  for (const [key, value] of Object.entries(LETTER_POOL)) {
+    copyPool[idx] = { [key]: value };
+    idx++;
+  }
+
+  while (hand.length < 10) {
+    const letterDrawn = Math.floor(Math.random() * 26);
+    const letterAndQuantity = copyPool[letterDrawn];
+
+    if (letterAndQuantity) {
+      for (const [letter, quantity] of Object.entries(letterAndQuantity)) {
+        if (quantity > 0) {
+          hand.push(letter);
+          letterAndQuantity[letter] -= 1;
+        }
+      }
+    }
+  }
+
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
