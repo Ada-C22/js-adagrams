e7e78d84583a65d7bef5e46f70366d0f993490b9
Ajar Duishembieva <ajduishe@gmail.com>
2024-11-27 14:33:51 -0800

    complete wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -72,8 +72,47 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
-};
+    const LETTER_SCORES = {
+        'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1,
+        'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
+        'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1,
+        'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1,
+        'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
+    };
+
+    let totalPoints = 0;
+
+    for (const letter of word.toUpperCase()) {
+        if (LETTER_SCORES[letter]) {
+            totalPoints += LETTER_SCORES[letter];
+        }
+    }
+
+    if (word.length >= 7 && word.length <= 10) {
+        totalPoints += 8;
+    }
+
+    return totalPoints;
+}
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
-};
+    let winningWord = '';
+    let winningScore = 0;
+
+    for (let word of words) {
+        let score = scoreWord(word);
+
+        if (winningScore === score) {
+              if (word.length === 10 && winningWord.length !== 10){
+                winningWord = word;
+              }else if (word.length < winningWord.length && winningWord.length !== 10){
+                winningWord = word;
+              }   
+            } else if (winningScore < score){
+              winningScore = score
+              winningWord = word
+            }
+          }
+          return {word: winningWord, score: winningScore}
+        };
