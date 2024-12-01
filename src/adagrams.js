67b045496c6a40397f6dd832e79837f3b3928fef
vlada <vladarap88@gmail.com>
2024-11-30 16:38:36 -0800

    completed wave 3 and 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -66,13 +66,73 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 // export default usesAvailableLetters;
 
 export const scoreWord = (word) => {
+  const scoreDict = {
+    A: 1,
+    E: 1,
+    I: 1,
+    O: 1,
+    U: 1,
+    L: 1,
+    N: 1,
+    R: 1,
+    S: 1,
+    T: 1,
+    D: 2,
+    G: 2,
+    B: 3,
+    C: 3,
+    M: 3,
+    P: 3,
+    F: 4,
+    H: 4,
+    V: 4,
+    W: 4,
+    Y: 4,
+    K: 5,
+    J: 8,
+    X: 8,
+    Q: 10,
+    Z: 10,
+  };
 
+  let totalScore = 0;
+
+  for (const letter of word.toUpperCase()) {
+    totalScore += scoreDict[letter] || 0;
+  }
+
+  if (word.length >= 7 && word.length <= 10) {
+    totalScore += 8;
+  }
+
+  return totalScore;
 };
 
-
+// export default scoreWord;
 
 export const highestScoreFrom = (words) => {
+  let highestScoringWord = {
+    word: "",
+    score: 0,
+  };
 
+  for (const word of words) {
+    const wordScore = scoreWord(word);
+
+    if (
+      wordScore > highestScoringWord.score ||
+      (wordScore === highestScoringWord.score &&
+        (word.length < highestScoringWord.word.length ||
+          (word.length === 10 && highestScoringWord.word.length !== 10)))
+    ) {
+      highestScoringWord = {
+        word: word,
+        score: wordScore,
+      };
+    }
+  }
+
+  return highestScoringWord;
 };
 
-
+// export default highestScoreFrom;
